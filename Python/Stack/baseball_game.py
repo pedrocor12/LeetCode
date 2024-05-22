091d487b6cef8634980c60a5a123491a1989dc9c
pedrocor12 <pinto7652@gmail.com>
2024-05-22 13:17:25 +0100

    +baseball_game stack question

---
+++ Python/Stack/baseball_game.py
@@ -0,0 +1,23 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        score_stack = []
+
+        for operation in operations:
+            if operation == "+":
+                summed = score_stack[-2] + score_stack[-1]
+                score_stack.append(summed)
+
+            elif operation == "D" and len(score_stack) >= 1:
+                doubled = score_stack[-1] * 2
+                score_stack.append(doubled)
+            
+            elif operation == "C" and len(score_stack) >= 1:
+                score_stack.pop()
+
+            else: 
+                score_stack.append(int(operation))
+
+        return sum(score_stack)
+
+# Time complexity : O(n)
+# Space complexity : O(n)
